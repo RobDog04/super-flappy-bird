b0f38025ebf74bca719661b71c133a446fb4aba5
kcwidman <kcwidman@gmail.com>
2021-03-27 12:17:43 -0500

    base file added and base scolling implemented

--- game.py
+++ game.py
@@ -1,33 +1,27 @@
-import pygame
 import sys
 from constants import *
 from bird import *
 from pipe import *
+from base import *
 
 pygame.init()
-def draw_floor():
-      SCREEN.blit(BASE_SURFACE,(FLOOR_X_POS, 800))
-      SCREEN.blit(BASE_SURFACE,(FLOOR_X_POS + 576, 800))
+pygame.display.set_caption("Flappy Bird")
+base = Base()
 
+# def game_loop():
+    
+def draw_window():
+    SCREEN.blit(BG_SURFACE,(0,0))
+    base.draw_floor()
+    pygame.display.update()
 
+run = True
+while run:
+    CLOCK.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    # game_loop()
+    draw_window()
 
-pygame.display.set_caption("Flappy Bird ")
-while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: 
-                pygame.quit()
-                sys.exit()
-        SCREEN.blit(BG_SURFACE,(0,0))
-        FLOOR_X_POS -= 1
-        draw_floor()
-        if FLOOR_X_POS <= -576:
-            FLOOR_X_POS = 0
-       
-       
-        pygame.display.update()
-        CLOCK.tick(120)              
-                
-        
-
-
-     
+pygame.quit()
