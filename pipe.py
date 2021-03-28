fb16d8e54b72d8e6e40e399303cd39bc87a28614
Charan Suresh <charan_s@ku.edu>
2021-03-28 13:10:07 -0500

    error fix

--- pipe.py
+++ pipe.py
@@ -3,8 +3,15 @@ class Pipe:
     top_h = 0   
     bot_h = 0
 
-x_loc-=VEL
+    tick_count = 0
 
-def __init__(self,h,b):
-        self.top_h = h
-        self.bot_h = b
+    def draw(self):
+        self.tick_count -= VEL
+        SCREEN.blit(PIPE_SURFACE,(self.tick_count + PIPE_WIDTH , HEIGHT))
+        SCREEN.blit(PIPE_SURFACE,(self.tick_count + WIDTH + PIPE_WIDTH, HEIGHT))
+        if self.tick_count <= -WIDTH:
+            self.tick_count = 0
+
+    def __init__(self,h,b):
+            self.top_h = h
+            self.bot_h = b
