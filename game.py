c777ea04eeccf5acfac8782f67dc25d009c250c1
Tani-Shah <tanishkashah24@gmail.com>
2021-03-30 16:41:41 -0500

    Added bird jumping and falling functionality. Will remove jumping and falling code once jump and fall functions in bird.py are complete. Game currently works.

--- game.py
+++ game.py
@@ -8,6 +8,7 @@ pygame.init()
 pygame.display.set_caption("Flappy Bird")
 base = Base()
 pipe = []
+
 def generate_pipes():
     #tes =0
     myPipe = Pipe(100,400)
@@ -22,16 +23,31 @@ def draw_window():
     base.draw()
     pygame.display.update()
 
-def main():
+
+def main(BIRD_Y_CHANGE, BIRD_INDEX):
     run = True
     while run:
         CLOCK.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT or event.type == GAME_OVER:
                 run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    #bird_jump()
+                    BIRD_Y_CHANGE = 0
+                    BIRD_Y_CHANGE -= 7.5
+            if event.type == BIRD_FLAP:
+                print("here")
+                BIRD_INDEX = (BIRD_INDEX + 1) % 2
+
         game_loop()
         draw_window()
+        #BIRD_MOVE = bird_fall(BIRD_Y_CHANGE)
+        BIRD_Y_CHANGE += 0.25
+        BIRD = animated_bird(BIRD_SURFACE, BIRD_Y_CHANGE)
+        BIRD_BOUNDARY.centery += BIRD_Y_CHANGE
+        draw_bird(BIRD, BIRD_BOUNDARY)
 
     pygame.quit()
 
-main()
+main(BIRD_Y_CHANGE, BIRD_INDEX)
