fd7bb67459b293184edbcf292fbeb4890273d50e
Tani-Shah <tanishkashah24@gmail.com>
2021-03-30 16:39:01 -0500

    Added more bird constants.

--- constants.py
+++ constants.py
@@ -20,3 +20,17 @@ PIPE_SURFACE = pygame.image.load("assets/pipe.png").convert()
 
 GENERATE_PIPE = pygame.USEREVENT
 GAME_OVER = pygame.USEREVENT + 1
+BIRD_FLAP = pygame.USEREVENT + 2 # For animating wings
+
+# Bird Constants
+BIRD_WIDTH = BIRD_HEIGHT = 50
+BIRD_X = 50
+BIRD_Y = int ( 576/2 - BIRD_HEIGHT/2 )
+BIRD_1 = pygame.image.load("assets/bird1.png").convert_alpha()
+BIRD_2 = pygame.image.load("assets/bird2.png").convert_alpha()
+BIRD_3 = pygame.image.load("assets/bird3.png").convert_alpha()
+BIRD = [BIRD_1, BIRD_2, BIRD_3]
+BIRD_INDEX = 0
+BIRD_SURFACE = BIRD[BIRD_INDEX]
+BIRD_BOUNDARY = BIRD_SURFACE.get_rect(center=(150,500))
+BIRD_Y_CHANGE = 0
