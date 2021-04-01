62fcb1a7bbae70e71188a561ccf51b60db391e4d
Tani-Shah <tanishkashah24@gmail.com>
2021-03-31 19:57:42 -0500

    Created class Bird and got functions working. Need to animate wing flapping

--- bird.py
+++ bird.py
@@ -1,26 +1,23 @@
 import pygame
 from constants import *
 
-'''
-def bird_fall(change):
-    change += 0.5
-    BIRD_BOUNDARY.centery += change
-    return BIRD_BOUNDARY
+class Bird:
 
-def bird_jump():
-    BIRD_Y_CHANGE = 0
-    BIRD_Y_CHANGE -= 10
-    print("flap")
-    return BIRD_Y_CHANGE
-'''
-def animated_bird(bird, move):
-   fallingBird = pygame.transform.rotozoom(bird, -move * 5, 1)
-   return fallingBird
+    def animate_bird(self, bird, move):
+        rotoBird = pygame.transform.rotozoom(bird, -move * 5, 1)
+        return rotoBird
 
-def draw_bird(bird, boundary):
+    def draw_bird(self, bird, boundary):
+        SCREEN.blit(bird, boundary)
 
-    SCREEN.blit(bird, boundary)
-    #base.draw()
-    pygame.display.update()
+    def bird_fall(self, change, boundary):
+        change += 0.25
+        boundary.centery += change
+        return change, boundary
+
+    def jump_bird(self, bird):
+        bird = 0
+        bird -= 7.5
+        return bird
 
 
