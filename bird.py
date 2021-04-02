882aec489bc300ebf7c679e82189286dcd3e45c8
RobDog04 <78656529+RobDog04@users.noreply.github.com>
2021-04-01 20:09:03 -0500

    Animations for the bird flapping have been added

--- bird.py
+++ bird.py
@@ -2,13 +2,20 @@ import pygame
 from constants import *
 
 class Bird:
-
+    birdDown = pygame.image.load('assets/bird3.png').convert_alpha()
+    birdCenter = pygame.image.load('assets/bird2.png').convert_alpha()
+    birdUp = pygame.image.load('assets/bird1.png').convert_alpha()
+    birdFrame = [birdDown, birdCenter, birdUp]
+    tick_count = 0
     falling_vel = 0
     rot_angle = 0
+    frame_index = 0
     boundary = BIRD_SURFACE.get_rect(center=(150,500))
-
-    def animate_bird(self):
-        rotoBird = pygame.transform.rotozoom(BIRD_SURFACE, max(-self.falling_vel * 5, -70), 1)
+      def animate_bird(self):
+        self.tick_count += 1
+        if self.tick_count % 3 == 0:
+            self.frame_index = (self.frame_index + 1) % 3
+        rotoBird = pygame.transform.rotozoom(self.birdFrame[self.frame_index], max(-self.falling_vel * 5, -70), 1)
         return rotoBird
 
     def draw_bird(self):
